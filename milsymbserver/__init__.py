09e3753bd42c655fd20b8a349a8dfd9d5b997847
Kjell Magne Fauske <kjellmf@gmail.com>
2015-04-06 18:00:40 +0200

    Start to experiment with SVG merging

--- milsymbserver/__init__.py
+++ milsymbserver/__init__.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
+import os
 from flask import Flask
 app = Flask(__name__)
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+app.config['JMSML_PATH'] = os.path.join(PROJECT_PATH, "../../joint-military-symbology-xml")
+app.config['SVG_PATH'] = os.path.abspath(os.path.join(app.config['JMSML_PATH'], 'svg/MIL_STD_2525D_Symbols'))
+
+try:
+    app.config.from_envvar('MILSYMBSERVER_SETTINGS')
+except RuntimeError:
+    app.logger.warning('Could not load config')
+    pass
+
 
 import milsymbserver.views
 
--- runserver.py
+++ runserver.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import logging
+import os
 
 from milsymbserver import app
 
 if __name__ == '__main__':
+    app.logger.setLevel(logging.DEBUG)
+    print os.path.abspath(app.config["SVG_PATH"])
     app.run(debug=True)
