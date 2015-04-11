35930235eb6460b7e53a6ba28d4e2ed7890f6059
Kjell Magne Fauske <kjellmf@gmail.com>
2015-04-11 18:10:21 +0200

    Start to work on symbol mappings

--- milsymbserver/jmsml.py
+++ milsymbserver/jmsml.py
@@ -10,7 +10,51 @@ def _load_jmsml_data(json_filepath):
     with open(json_filepath) as f:
         _JMSML = json.load(f)
 
+
+    print _JMSML["affiliations"]
+
+
+class SymbolMappings(object):
+    def __init__(self, jmsml_data):
+        self._data = jmsml_data
+        self._create_standard_identity_mappings()
+        self._create_context_mappings()
+        self._create_symbolset_mappings()
+
+    def _create_standard_identity_mappings(self):
+        self._si_mappings = {}
+        for si in self._data["standardIdentities"]:
+            self._si_mappings[si["digits"]] = si["id"]
+
+    def _create_context_mappings(self):
+        self._context_mappings = {}
+        for context in self._data["contexts"]:
+            self._context_mappings[context["digits"]] = context["id"]
+
+    @property
+    def standard_identity(self):
+        return self._si_mappings
+
+    @property
+    def context(self):
+        return self._context_mappings
+
+    def _create_symbolset_mappings(self):
+        self._symbol_set_mappings = {}
+        for symbol_set in self._data["symbolSets"]:
+            self._symbol_set_mappings[symbol_set["digits"]] = symbol_set
+
+    @property
+    def symbol_sets(self):
+        return self._symbol_set_mappings
+
+
+
+
 _load_jmsml_data(app.config["DATA_FILE"])
+mappings = SymbolMappings(_JMSML)
+
+
 
 
 class InvalidSidcLength(Exception):
@@ -43,9 +87,11 @@ class Sidc(object):
 
 
 class MilSymbol(object):
+    """Representation of a MILSTD 2525D/APP6D symbol"""
     def __init__(self, sidc):
         self.sidc = Sidc(sidc)
         self._frame_fn = None
+        self._init_ids()
 
     @property
     def frame_fn(self):
@@ -54,7 +100,21 @@ class MilSymbol(object):
         return self._frame_fn
 
     def _find_frame_fn(self):
-        self._frame_fn = "TTTTT.SVG"
+        affiliations = _JMSML["affiliations"]
+        frame_data = affiliations[self._context_id][self._dimension_id][self._standard_identity_id]
+        if self.sidc.status == "1" and "plannedGraphic" in frame_data:
+            self._frame_fn = frame_data["plannedGraphic"]
+        else:
+            self._frame_fn = frame_data["graphic"]
+
+    def _init_ids(self):
+        self._context_id = mappings.context.get(self.sidc.context)
+        self._standard_identity_id = mappings.standard_identity.get(self.sidc.standard_identity)
+        self._symbol_set = mappings.symbol_sets.get(self.sidc.symbolset)
+        self._dimension_id = self._symbol_set.get("dimensionId")
+
+
+
 
 
 
